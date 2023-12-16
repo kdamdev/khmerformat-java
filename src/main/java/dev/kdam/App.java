cae1810b646bdca396e91537a43cdafb708488d2
kheangcheang <kheangcheang855@hotmail.com>
2023-12-16 22:35:28 +0700

    config compile in pom

--- src/main/java/dev/kdam/App.java
+++ src/main/java/dev/kdam/App.java
@@ -2,11 +2,6 @@ package dev.kdam;
 
 import dev.kdam.utils.KhmerLunarDateTime;
 import dev.kdam.utils.KhmerNumeric;
-import dev.kdam.utils.KhmerSolarDateTime;
-
-import java.math.BigDecimal;
-import java.time.chrono.ThaiBuddhistDate;
-import java.time.format.DateTimeFormatter;
 
 /**
  * Hello world!
@@ -16,8 +11,8 @@ public class App
 {
     public static void main( String[] args ) {
         //System.out.println( "Hello World!" );
-        KhmerSolarDateTime solar = new KhmerSolarDateTime(01,12,2023);
-        System.out.println( solar.toString());
+        //KhmerSolarDateTime solar = new KhmerSolarDateTime(01,12,2023);
+        //System.out.println( solar.toString());
         System.out.println( "-----------------" );
         KhmerNumeric numeric = new KhmerNumeric("123456789012345");
         System.out.println( numeric.toKhmer(true));
