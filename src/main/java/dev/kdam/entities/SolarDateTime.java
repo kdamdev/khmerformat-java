46b3ee53059a52693ef7faa12618017539c4891e
kheangcheang <kheangcheang855@hotmail.com>
2023-12-18 08:48:24 +0700

    add commit Solar date

---
+++ src/main/java/dev/kdam/entities/SolarDateTime.java
@@ -0,0 +1,44 @@
+package dev.kdam.entities;
+
+/**
+ * SolarDateTime
+ */
+public class SolarDateTime {
+    private String day;
+    private String month;
+    private String year;
+
+    public SolarDateTime(String day, String month, String year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public SolarDateTime() {
+
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+}
---
+++ src/main/java/dev/kdam/utils/KhmerSolarDateTime.java
@@ -0,0 +1,26 @@
+package dev.kdam.utils;
+
+import dev.kdam.constraints.SolarMonth;
+import dev.kdam.entities.SolarDateTime;
+
+/**
+ *
+ */
+public class KhmerSolarDateTime {
+    private final SolarDateTime solar;
+    public KhmerSolarDateTime(int day, int month, int year) {
+        this.solar = new SolarDateTime();
+
+        this.solar.setDay( new KhmerNumeric(day).toKhmer());
+        this.solar.setMonth( SolarMonth.month[--month] );
+        this.solar.setYear( new KhmerNumeric(year).toKhmer());
+    }
+
+    public KhmerSolarDateTime() {
+        this.solar = new SolarDateTime();
+    }
+
+    public String toString() {
+        return String.format( "ថ្ងៃទី%s ខែ%s ឆ្នាំ%s" , this.solar.getDay(), this.solar.getMonth(), this.solar.getYear());
+    }
+}
