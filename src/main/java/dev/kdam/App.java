5be2d95975eac3851ad8322a495b5b8fb1529725
kheangcheang <kheangcheang855@hotmail.com>
2023-12-20 16:49:55 +0700

    add

--- src/main/java/dev/kdam/App.java
+++ src/main/java/dev/kdam/App.java
@@ -22,50 +22,26 @@ public class App
 //        KhmerNumeric numeric = new KhmerNumeric("123456789012345");
 //        System.out.println( numeric.toKhmer(true));
 //        System.out.println( numeric.toKhmer());
-//        System.out.println( numeric.toKhmerText() );
+//        2008
 //        System.out.println( "-----------------" );
 
-        KhmerLunarDateTime lunar = new KhmerLunarDateTime(1,1,1900);
-        System.out.println(lunar.toString());
-        System.out.println(lunar.calculateSoryaLeangsak().get( 0 ).getLesserEra());
-        System.out.println(lunar.calculateSoryaLeangsak().get( 0 ).getHarkun());
-        System.out.println(lunar.calculateSoryaLeangsak().get( 0 ).getKromathopol());
-        System.out.println(lunar.calculateSoryaLeangsak().get( 0 ).getAvaman());
-        System.out.println(lunar.calculateSoryaLeangsak().get( 0 ).getBodethey());
+        KhmerLunarDateTime lunar = new KhmerLunarDateTime( 1, 1, 2023 );
+        System.out.println( lunar.toString() );
+        System.out.println( lunar.calculateSoryaLeangsak().get( 0 ).getLesserEra() );
+        System.out.println( lunar.calculateSoryaLeangsak().get( 0 ).getHarkun() );
+        System.out.println( lunar.calculateSoryaLeangsak().get( 0 ).getKromathopol() );
+        System.out.println( lunar.calculateSoryaLeangsak().get( 0 ).getAvaman() );
+        System.out.println( lunar.calculateSoryaLeangsak().get( 0 ).getBodethey() );
         System.out.println( "-----------------" );
-        System.out.println(lunar.calculateSoryaLeangsak().get( 1 ).getLesserEra());
-        System.out.println(lunar.calculateSoryaLeangsak().get( 1 ).getHarkun());
-        System.out.println(lunar.calculateSoryaLeangsak().get( 1 ).getKromathopol());
-        System.out.println(lunar.calculateSoryaLeangsak().get( 1 ).getAvaman());
-        System.out.println(lunar.calculateSoryaLeangsak().get( 1 ).getBodethey());
+        System.out.println( lunar.calculateSoryaLeangsak().get( 1 ).getLesserEra() );
+        System.out.println( lunar.calculateSoryaLeangsak().get( 1 ).getHarkun() );
+        System.out.println( lunar.calculateSoryaLeangsak().get( 1 ).getKromathopol() );
+        System.out.println( lunar.calculateSoryaLeangsak().get( 1 ).getAvaman() );
+        System.out.println( lunar.calculateSoryaLeangsak().get( 1 ).getBodethey() );
         System.out.println( "-----------------" );
-        System.out.println(lunar.getDayOfMonth(20));
-    }
+        System.out.println( lunar.isAkthimeas() );
+        ///
 
-    public static String calculateDayOfWeekBuiltIn(int year, int month, int day) {
-        LocalDate date = LocalDate.of(year, month, day);
-        DayOfWeek dayOfWeek = date.getDayOfWeek();
-        return dayOfWeek.toString();
-    }
 
-    public static String calculateDayOfWeek(int year, int month, int day) {
-        String[] daysOfWeek = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-
-        // Adjusting January and February for Zeller's Congruence
-        if (month < 3) {
-            month += 12;
-            year--;
-        }
-
-        int century = year / 100;
-        year %= 100;
-
-        // Zeller's Congruence formula without month adjustment
-        int dayOfWeek = (day + ((13 * (month + 1)) / 5) + year + (year / 4) + (century / 4) - (2 * century)) % 7;
-
-        // Adjusting for Java's DayOfWeek constants (Sunday = 0, Monday = 1, ..., Saturday = 6)
-        dayOfWeek = (dayOfWeek + 6) % 7; // Shifting to align with Java's DayOfWeek
-
-        return daysOfWeek[dayOfWeek];
     }
 }
--- src/main/java/dev/kdam/Helper/KhmerLunarDateTime.java
+++ src/main/java/dev/kdam/Helper/KhmerLunarDateTime.java
@@ -1,13 +1,13 @@
 package dev.kdam.Helper;
 
+import dev.kdam.Entities.LunarDateTime;
+import dev.kdam.Entities.SoryaLeangsak;
 import dev.kdam.Utils.AnimalYear;
 import dev.kdam.Utils.DayOfWeek;
 import dev.kdam.Utils.Era;
-import dev.kdam.Entities.LunarDateTime;
-import dev.kdam.Entities.SoryaLeangsak;
 import dev.kdam.Utils.JourneyMoon;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,11 +15,16 @@ import java.util.List;
  * KhmerLunarDateTime
  */
 public class KhmerLunarDateTime {
-    private LunarDateTime lunar;
-    private LocalDateTime localDateTime;
+    private final LunarDateTime lunar;
+    private final LocalDate localDate;
+    private final SoryaLeangsak xLesserEra;
+    private final SoryaLeangsak zLesserEra;
     public KhmerLunarDateTime(int day, int month, int year) {
         this.lunar = new LunarDateTime();
-        localDateTime = LocalDateTime.of( year, month, day,0,0,0 );
+        localDate = LocalDate.of( year, month, day);
+        //initial Lesser Era
+        this.xLesserEra = getSoryaLeangsakByLesserEra( (year + 543) - 1182  );
+        this.zLesserEra = getSoryaLeangsakByLesserEra( (year + 544) - 1182  );
     }
     public int calculateLesserEra() {
         return 0;
@@ -41,16 +46,30 @@ public class KhmerLunarDateTime {
         int t = day % 15 == 0 ? 15 : day % 15;
         return t + " " + (day > 15 ? JourneyMoon.WANING.getLabel() : JourneyMoon.WAXING.getLabel());
     }
-
+    public boolean is366KhmerSolar() {
+        return xLesserEra.getKromathopol() <= 207;
+    }
     public boolean isAkthimeas() {
-        if(calculateSoryaLeangsak().get(0).getBodethey() > 24 || calculateSoryaLeangsak().get(0).getBodethey() < 6)
+        if (xLesserEra.getBodethey() > 24 || zLesserEra.getBodethey() < 6)
             return true;
+//        else if (xLesserEra.getBodethey() == 24 && ze) {
+//
+//        }
         return false;
+    }
+    public boolean isChantreathimeas(int kromathopol, int avoman, int newavoman){
+//        if(is366KhmerSolar(kromathopol)){
+//            return avoman < 127;
+//        }else{
+//            return avoman < 138 && avoman == 137 && newavoman == 0;
+//        }
+        return false;
+
     }
     public List<SoryaLeangsak> calculateSoryaLeangsak() {
         List<SoryaLeangsak> lst = new ArrayList<>();
-        lst.add( this.getSoryaLeangsakByLesserEra( this.localDateTime.getYear() + 543 - 1182 ) );
-        lst.add( this.getSoryaLeangsakByLesserEra( this.localDateTime.getYear() + 544 - 1182 ) );
+        lst.add( this.getSoryaLeangsakByLesserEra( this.localDate.getYear() + 543 - 1182 ) );
+        lst.add( this.getSoryaLeangsakByLesserEra( this.localDate.getYear() + 544 - 1182 ) );
 
         return lst;
     }
@@ -68,21 +87,21 @@ public class KhmerLunarDateTime {
      * @return String
      */
     public String calAnimalYear() {
-        return AnimalYear.year[(this.localDateTime.getYear() + 9) % 12];
+        return AnimalYear.year[(this.localDate.getYear() + 9) % 12];
     }
 
     /**
      * @return String
      */
     public String calEra() {
-        return Era.sak[(this.localDateTime.getYear() + 2) % 10];
+        return Era.sak[(this.localDate.getYear() + 2) % 10];
     }
 
     /**
      * @return
      */
     private String calDayOfWeek() {
-        return DayOfWeek.day_of_week[this.localDateTime.getDayOfWeek().getValue()];
+        return DayOfWeek.day_of_week[0];
     }
     public String toString() {
         return String.format( "ថ្ងៃ%s %s ខែ%s ឆ្នាំ%s %s ព.ស.%s", this.calDayOfWeek(), this.lunar.getDayOfWeek(),this.lunar.getDayOfWeek(), this.calAnimalYear(), this.calEra(), this.lunar.getDayOfWeek());
